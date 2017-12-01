0d0a8e172a58a74a9b33675cca922b28ced5a391
Alquin Gayle <Alquin.Gayle@ca.com>
2017-12-01 16:11:20 -0500

    Added functions for computer system id, niscache clear, device id reset and restart

--- cauimws.py
+++ cauimws.py
@@ -4,9 +4,12 @@ REST API
 '''
 from __future__ import print_function
 import logging
+from socket import gethostbyname, gaierror
 from json import loads, dumps
+from re import search
 from requests import get, post, put, ConnectionError
 from requests.auth import HTTPBasicAuth
+
 
 def get_hubs(ws_info):
     '''Obtains a list of hubs from the UIM domain
@@ -457,3 +460,245 @@ def get_qos_sources(ws_info, qos):
         logging.exception('Failed to call API to get sources for %s QOS', qos)
 
     return sources
+
+
+def _get_ip(device):
+    '''Obtains the IP address of device
+
+    Args:
+        device (string) name to get an IP address for
+
+    Returns:
+        ip_addr (string) ip address of device or None on failure
+    '''
+
+    ip_addr = None
+    try:
+        ip_addr = gethostbyname(device)
+        logging.debug('IP address for %s is %s', device, ip_addr)
+    except gaierror:
+        logging.exception('Failed to call gethostbyname for %s', device)
+
+    return ip_addr
+
+
+def get_comp_sys_ids(ws_info, device):
+    '''Obtains the computer system id for the device from the UIM API
+
+    API Links:
+        https://docops.ca.com/ca-unified-infrastructure-management-probes/ga/en/probe-development-tools/restful-web-services/call-reference/computer-system-calls#ComputerSystemCalls-GetComputerSystemDetails(UsingtheIPAddress)
+        https://docops.ca.com/ca-unified-infrastructure-management-probes/ga/en/probe-development-tools/restful-web-services/call-reference/computer-system-calls#ComputerSystemCalls-GetComputerSystemDetails(UsingtheCSName)
+
+    Args:
+        ws_info (dict) containing
+            user (string) UIM user with web service access
+            password (string) UIM user password
+            url (string) UIM REST API URL
+        device (string) to get the computer system id(s) for
+
+    Returns:
+        cs_id (list) of the computer system id(s)
+    '''
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+
+    cs_id = []
+    ip_addr = _get_ip(device)
+    if ip_addr:
+        url = ws_info['url'] + '/computer_systems/cs_ip/' + ip_addr
+
+        try:
+            response = get(
+                url,
+                auth=HTTPBasicAuth(
+                    ws_info['user'],
+                    ws_info['password']
+                ),
+                verify=False,
+                headers=headers)
+
+            logging.debug(
+                'The response from the get computer system id by IP was %s',
+                response.text
+            )
+            if response.status_code == 200:
+                results = loads(response.text)
+
+                # UIM sometimes has duplicate devices not correlated by discovery_server
+                for computer_system in results['computer_system']:
+                    cs_id.append(computer_system['cs_id'])
+
+                logging.info(
+                    'Successfully got the computer system id for %s by IP as %s',
+                    device,
+                    str(cs_id))
+            else:
+                logging.warning(
+                    'Failed to get computer system id for %s. Response code was %s',
+                    device,
+                    response.status_code
+                )
+
+        except ConnectionError:
+            logging.exception('Failed to call UIM computer system id by IP API')
+
+        except KeyError:
+            logging.exception('No computer systems found by IP for name %s', device)
+    else:
+        url = ws_info['url'] + '/computer_systems/cs_name/' + device
+
+        try:
+            response = get(
+                url,
+                auth=HTTPBasicAuth(
+                    ws_info['user'],
+                    ws_info['password']
+                ),
+                verify=False,
+                headers=headers
+            )
+            logging.debug(
+                'The response from the get computer system id by name was %s',
+                response.text
+            )
+            if response.status_code == 200:
+                results = loads(response.text)
+                for computer_system in results['computer_system']:
+                    cs_id.append(computer_system['cs_id'])
+
+                logging.info(
+                    'Successfully got the computer system id for %s by name as %s',
+                    device,
+                    str(cs_id)
+                )
+
+            else:
+                logging.warning(
+                    'Failed to get computer system id for %s by name. Response code was %s',
+                    device,
+                    response.status_code
+                )
+
+        except ConnectionError:
+            logging.exception('Failed to call UIM computer system id by name API')
+
+        except KeyError:
+            logging.exception('No computer systems found by name for name %s', device)
+
+    return cs_id
+
+
+def nis_cache_clean(ws_info, robot_address):
+    '''Clears the robot niscache using the _nis_cache_clean callback
+
+    API Link: https://docops.ca.com/ca-unified-infrastructure-management-probes/ga/en/probe-development-tools/restful-web-services/call-reference/probe-calls#ProbeCalls-InvokeCallback
+
+    Args:
+        ws_info (dict) containing
+            user (string) UIM user with web service access
+            password (string) UIM user password
+            url (string) UIM REST API URL
+            domain (string) UIM domain name
+        robot_address (string) of the address to the UIM robot
+
+    Returns:
+        True if successfull in running cleaning the nis cache, False if not
+    '''
+    url = ws_info['url'] + '/probe' + robot_address + '/controller/callback/_nis_cache_clean'
+    headers = {
+        'content-type': 'application/json',
+        'accept': 'application/json'
+    }
+    data = {
+        'parameters' : {
+            'name': 'robot',
+            'type': 'string',
+            'value': ''
+        }
+    }
+    found = search(r'/.+/.+/(.+)$', robot_address)
+    robot = found.group(1)
+    data['parameters']['value'] = robot
+
+    result = False
+    try:
+        response = post(
+            url,
+            auth=HTTPBasicAuth(
+                ws_info['user'],
+                ws_info['password']
+            ),
+            verify=False,
+            headers=headers,
+            data=dumps(data)
+        )
+        logging.debug('The response from callback _nis_cache_clean was %s', response.text)
+        if response.status_code == 200:
+            result = True
+        else:
+            logging.error('Failed to clean nis cache for robot %s', robot_address)
+
+    except ConnectionError:
+        logging.exception('Failed to callback _nis_cache_clean API')
+
+    return result
+
+
+def reset_device_id_and_restart(ws_info, robot_address):
+    '''Resets the robot device id and restarts the robot
+
+    API Link: https://docops.ca.com/ca-unified-infrastructure-management-probes/ga/en/probe-development-tools/restful-web-services/call-reference/probe-calls#ProbeCalls-InvokeCallback
+
+    Args:
+        ws_info (dict) containing
+            user (string) UIM user with web service access
+            password (string) UIM user password
+            url (string) UIM REST API URL
+            domain (string) UIM domain name
+        robot_address (string) of the address to the UIM robot
+
+    Returns:
+        True if successfull in reseting the device id and restarting, False if not
+    '''
+    url = ws_info['url'] + '/probe' + robot_address + \
+        '/controller/callback/_reset_device_id_and_restart'
+    headers = {
+        'content-type': 'application/json',
+        'accept': 'application/json'
+    }
+    data = {
+        'parameters' : {
+            'name': 'robot',
+            'type': 'string',
+            'value': ''
+        }
+    }
+
+    found = search(r'/.+/.+/(.+)$', robot_address)
+    robot = found.group(1)
+    data['parameters']['value'] = robot
+
+    result = False
+    try:
+        response = post(
+            url,
+            auth=HTTPBasicAuth(
+                ws_info['user'],
+                ws_info['password']
+            ),
+            verify=False,
+            headers=headers,
+            data=dumps(data)
+        )
+        logging.debug('The response from callback _reset_device_id_and_restart was %s', response.text)
+        if response.status_code == 200:
+            result = True
+        else:
+            logging.error('Failed to reset device id and restart for robot %s', robot_address)
+
+    except ConnectionError:
+        logging.exception('Failed to callback _reset_device_id_and_restart API')
+
+    return result
